7608087571d346f6078ea6c7634a974aa51e45fb
Andy Feliciotti <someguy9@gmail.com>
2013-05-27 12:35:17 -0400

    Made the example json more dynamic

--- sayt-json.php
+++ sayt-json.php
@@ -1,20 +1,29 @@
 <?php
 
+$query = htmlspecialchars($_GET['query']);
+$callback = htmlspecialchars($_GET['callback']);
+
+$countries = array("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
+ 
+$searchresults = preg_grep("/.*".$query.".*/i",$countries);
+
+$i = 0;
+while (list($key, $value) = each($searchresults)) {
+	$i++;
+	$searchresults2[]['title'] = $value;
+	
+	if($i == 3 ){break;};
+}
+$searchresults2count = count($searchresults);
+
 $data = '[
     {
         "section": {
-            "title": "Pages",
-            "num": "1",
-            "limit": "1"
+            "title": "Countries",
+            "num": "'.$searchresults2count.'",
+            "limit": "3"
         },
-        "data": [
-            {
-                "title": "Andy Feliciottis Portfolio",
-                "image": "http://static.drawne.com/assets/img/logo1.png",
-                "description": "See more work by Andy",
-                "url": "http://drawne.com"
-            }
-        ]
+        "data": '.json_encode($searchresults2).'
     },
     {
         "section": {
@@ -24,16 +33,16 @@ $data = '[
         },
         "data": [
             {
-                "title": "Google Search for '.$_GET['query'].'",
-                "url": "https://www.google.com/#q='.$_GET['query'].'&fp=1"
+                "title": "Google Search for '.$query.'",
+                "url": "https://www.google.com/#q='.$query.'&fp=1"
             },
             {
-                "title": "Bing Search for '.$_GET['query'].'",
-                "url": "http://www.bing.com/search?q='.$_GET['query'].'"
+                "title": "Bing Search for '.$query.'",
+                "url": "http://www.bing.com/search?q='.$query.'"
             },
             {
-                "title": "Yahoo Search for '.$_GET['query'].'",
-                "url": "http://search.yahoo.com/search;_ylt=AjjCFSene_qdvfjfH51XBWebvZx4?p='.$_GET['query'].'"
+                "title": "Yahoo Search for '.$query.'",
+                "url": "http://search.yahoo.com/search;_ylt=AjjCFSene_qdvfjfH51XBWebvZx4?p='.$query.'"
             }
         ]
     },
@@ -80,7 +89,7 @@ if(array_key_exists('callback', $_GET)){
     header('Access-Control-Max-Age: 3628800');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
-    $callback = $_GET['callback'];
+    $callback = $callback;
     echo $callback.'('.$data.');';
 
 }else{
